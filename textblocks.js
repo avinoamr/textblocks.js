ce8f0f3fb6fcd634c3922a503fd87bf18b46c04b
Roi Avinoam <avinoamr@gmail.com>
2012-07-16 00:59:13 +0300

    (1) Fixes the breaks from the reversing the block components order. (2) Cleanup the key events code, to make it more readable. The auto positioning is still bugged out. Work in progress

--- textblocks.js
+++ textblocks.js
@@ -1,9 +1,40 @@
 /**
+ * textblocks.js
+ * 
+ * Customized text box input for interactive UI experiences
+ * https://github.com/avinoamr/textblocks.js
  *
+ * The MIT License
+ * 
+ * Copyright (c) 2010-2012 Roi Avinoam <avinoamr@gmail.com> and jquery-selection authors.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
- *
+ * Uses: Rangy Text Inputs (MIT License) by Tim Down (http://code.google.com/p/rangy/). See below.
  */
 (function( $ ) {
+
+    // key constants
+    var KEY_LEFT = 37,
+        KEY_RIGHT = 39,
+        KEY_BACKSPACE = 8,
+        KEY_DELETE = 46;
 
     //
     jQuery.fn.textblocks = function( settings ) {
@@ -62,11 +93,16 @@
 
                     // dynamic position
                     var height_compare = parent;
-                    var next = parent.next();
-                    if ( !element && next.length ) {
-                        height_compare = next;
+                    if ( height_compare.prev().length ) {
+                        height_compare = height_compare.prev().first();
+                        console.log( 'prev' );
+                    } else if ( height_compare.next().length ) {
+                        height_compare = height_compare.next().first();
+                        console.log( 'next' );
+                    } else {
+                        console.log( 'parent' );
                     }
-                    height_compare = height_compare.children().first();
+                    height_compare = height_compare.children().last();
 
                     var top = parseInt( height_compare.css( 'margin-top' ) ) + 
                         ( ( height_compare.innerHeight() - $this.innerHeight() ) / 2 );
@@ -99,47 +135,73 @@
 
                 // handle traversal a long the block values
                 .on( 'keydown', function( ev ) {
-                    
-                    // move backwards (left-arrow or backspace)
-                    if ( 0 == text[ 0 ].selectionStart && ( 8 == ev.keyCode || 37 == ev.keyCode ) ) {
 
-                        var parent = $( this ).parent();
-                        var prev = parent.prev(), val;
+                    // get the selection data
+                    var selection = $( this ).getSelection();
+                    if ( selection.start != selection.end ) {
+                        return; // disregard selection ranges
+                    }
+                    var cursor = selection.start;
+                    var $this = $( this );
+                    var end = $this.val().length;
 
-                        // remove previous (backspace)?
-                        if ( 8 == ev.keyCode && parent.siblings().length ) {
+                    // move backwards (left-key)
+                    if ( 0 == cursor && KEY_LEFT == ev.keyCode ) {
 
-                            val = parent.prev().attr( 'data-value' ); // + parent.find( 'input[ type="text" ]' ).val();
-                            parent.prev().detach();
-                            ev.preventDefault();
-
-                        }
-
-                        // jump backwards
-                        var input = parent.find( 'input[ type="text" ]' )
-                        val && ( input.val( val + input.val() ) );
-                        
-                        input.focus();
+                        // find and focus on text input box of the previous block
+                        $this.parent().prev().find( 'input[ type="text" ]' ).focus();
 
                     }
 
-                    // move forward (right-arrow or delete)
-                    if ( $( this ).val().length == text[ 0 ].selectionEnd && ( 46 == ev.keyCode || 39 == ev.keyCode ) ) {
+                    // move forward (right-key)
+                    if ( end == cursor && KEY_RIGHT == ev.keyCode ) {
 
-                        var parent = $( this ).parent();
+                        // find and focus on the text input box of the next block
+                        $this.parent().next().find( 'input[ type="text" ]' ).focus();
+
+                    }
+
+                    // remove the previous block (backspace)
+                    if ( 0 == cursor && KEY_BACKSPACE == ev.keyCode ) {
+
+                        var parent = $this.parent();
+                        var prev = parent.prev();
+                        prev.detach(); // remove the previous block
+
+                        // read the values of the removed text box, block and the current input box
+                        var text_val = prev.find( 'input[ type="text" ]' ).val();
+                        var val = prev.attr( 'data-value' );
+                        var input = parent.find( 'input[ type="text" ]' );
+
+                        // set the input value of the current block
+                        input
+                            .val( text_val + val + input.val() )
+                            .setSelection( text_val.length + val.length, text_val.length + val.length )
+                            .focus();
+
+                        ev.preventDefault(); // this backspace shouldn't remove any characters
+
+                    }
+
+                    // remove the next block (delete)
+                    if ( end == cursor && KEY_DELETE == ev.keyCode ) {
+
+                        var parent = $this.parent();
                         var next = parent.next();
+                        parent.detach(); // remove this block (practically the next)
 
-                        // remove next (delete)
-                        if ( 46 == ev.keyCode ) {
-                            var val = next.attr( 'data-value' );
-                            next.detach();
-                            $( this ).val( $( this ).val() + val ).focus();
-                        } else {
-                            next.find( 'input[ type="text" ]' )
-                                .focus();
-                        }
+                        // read the values of the removed text box, block and the next input box
+                        var text_val = $this.val();
+                        var val = parent.attr( 'data-value' );
+                        var input = next.find( 'input[ type="text" ]' );
 
+                        // set the input value of the next block
+                        input
+                            .val( text_val + val + input.val() )
+                            .setSelection( text_val.length, text_val.length )
+                            .focus();
 
+                        ev.preventDefault(); // this delete shouldn't remove any characters
 
                     }
 
@@ -167,4 +229,26 @@
 
     };
 
-})( jQuery )
+})( jQuery );
+
+
+/*
+ Rangy Text Inputs, a cross-browser textarea and text input library plug-in for jQuery.
+
+ Part of Rangy, a cross-browser JavaScript range and selection library
+ http://code.google.com/p/rangy/
+
+ Depends on jQuery 1.0 or later.
+
+ Copyright 2010, Tim Down
+ Licensed under the MIT license.
+ Version: 0.1.205
+ Build date: 5 November 2010
+*/
+(function(n){function o(e,g){var a=typeof e[g];return a==="function"||!!(a=="object"&&e[g])||a=="unknown"}function p(e,g,a){if(g<0)g+=e.value.length;if(typeof a=="undefined")a=g;if(a<0)a+=e.value.length;return{start:g,end:a}}function k(){return typeof document.body=="object"&&document.body?document.body:document.getElementsByTagName("body")[0]}var i,h,q,l,r,s,t,u,m;n(document).ready(function(){function e(a,b){return function(){var c=this.jquery?this[0]:this,d=c.nodeName.toLowerCase();if(c.nodeType==
+1&&(d=="textarea"||d=="input"&&c.type=="text")){c=[c].concat(Array.prototype.slice.call(arguments));c=a.apply(this,c);if(!b)return c}if(b)return this}}var g=document.createElement("textarea");k().appendChild(g);if(typeof g.selectionStart!="undefined"&&typeof g.selectionEnd!="undefined"){i=function(a){return{start:a.selectionStart,end:a.selectionEnd,length:a.selectionEnd-a.selectionStart,text:a.value.slice(a.selectionStart,a.selectionEnd)}};h=function(a,b,c){b=p(a,b,c);a.selectionStart=b.start;a.selectionEnd=
+b.end};m=function(a,b){if(b)a.selectionEnd=a.selectionStart;else a.selectionStart=a.selectionEnd}}else if(o(g,"createTextRange")&&typeof document.selection=="object"&&document.selection&&o(document.selection,"createRange")){i=function(a){var b=0,c=0,d,f,j;if((j=document.selection.createRange())&&j.parentElement()==a){f=a.value.length;d=a.value.replace(/\r\n/g,"\n");c=a.createTextRange();c.moveToBookmark(j.getBookmark());j=a.createTextRange();j.collapse(false);if(c.compareEndPoints("StartToEnd",j)>
+-1)b=c=f;else{b=-c.moveStart("character",-f);b+=d.slice(0,b).split("\n").length-1;if(c.compareEndPoints("EndToEnd",j)>-1)c=f;else{c=-c.moveEnd("character",-f);c+=d.slice(0,c).split("\n").length-1}}}return{start:b,end:c,length:c-b,text:a.value.slice(b,c)}};h=function(a,b,c){b=p(a,b,c);c=a.createTextRange();var d=b.start-(a.value.slice(0,b.start).split("\r\n").length-1);c.collapse(true);if(b.start==b.end)c.move("character",d);else{c.moveEnd("character",b.end-(a.value.slice(0,b.end).split("\r\n").length-
+1));c.moveStart("character",d)}c.select()};m=function(a,b){var c=document.selection.createRange();c.collapse(b);c.select()}}else{k().removeChild(g);window.console&&window.console.log&&window.console.log("TextInputs module for Rangy not supported in your browser. Reason: No means of finding text input caret position");return}k().removeChild(g);l=function(a,b,c,d){var f;if(b!=c){f=a.value;a.value=f.slice(0,b)+f.slice(c)}d&&h(a,b,b)};q=function(a){var b=i(a);l(a,b.start,b.end,true)};u=function(a){var b=
+i(a),c;if(b.start!=b.end){c=a.value;a.value=c.slice(0,b.start)+c.slice(b.end)}h(a,b.start,b.start);return b.text};r=function(a,b,c,d){var f=a.value;a.value=f.slice(0,c)+b+f.slice(c);if(d){b=c+b.length;h(a,b,b)}};s=function(a,b){var c=i(a),d=a.value;a.value=d.slice(0,c.start)+b+d.slice(c.end);c=c.start+b.length;h(a,c,c)};t=function(a,b,c){var d=i(a),f=a.value;a.value=f.slice(0,d.start)+b+d.text+c+f.slice(d.end);b=d.start+b.length;h(a,b,b+d.length)};n.fn.extend({getSelection:e(i,false),setSelection:e(h,
+true),collapseSelection:e(m,true),deleteSelectedText:e(q,true),deleteText:e(l,true),extractSelectedText:e(u,false),insertText:e(r,true),replaceSelectedText:e(s,true),surroundSelectedText:e(t,true)})})})(jQuery);
