4f01e0e78fb9f69b6d1dcd06e0ba02d4e840e712
Roi Avinoam <avinoamr@gmail.com>
2012-08-25 20:53:35 +0300

    The comment was a bit off

--- textblocks.js
+++ textblocks.js
@@ -6,6 +6,7 @@
  *
  * TODO
  *   1. Support range selection across multiple blocks, plus Ctrl+A
+ *   2. Support Undo & Redo
  *
  * THANKS
  *   Rangy Text Inputs (MIT License) by Tim Down (http://code.google.com/p/rangy/)
@@ -13,7 +14,7 @@
  *
  * The MIT License
  * 
- * Copyright (c) 2010-2012 Roi Avinoam <avinoamr@gmail.com> and jquery-selection authors.
+ * Copyright (c) 2010-2012 Roi Avinoam <avinoamr@gmail.com> and textblocks authors.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -32,8 +33,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *
- * Uses: . See below.
+ * 
  */
 (function( $ ) {
 
